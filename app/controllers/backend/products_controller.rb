763bb84a991d7c6c7e3f77fe357126b7bdf24daf
wuwood <kilokingw@gmail.com>
2015-04-08 01:06:03 +0800

    Contorller and Backend Product Views

--- app/controllers/backend/products_controller.rb
+++ app/controllers/backend/products_controller.rb
@@ -1,7 +1,9 @@
 class Backend::ProductsController < Backend::BaseController
-
+  before_action :find_product, only:[:show, :edit, :update, :destroy]
+  
   def index
     # TODO
+    @product = Product.all
   end
 
   def new
@@ -23,11 +25,27 @@ class Backend::ProductsController < Backend::BaseController
 
   def update
     # TODO
+      if @product.update(product_params)
+
+        redirect_to backend_products_path
+      else
+        render :edit
+      end
+
   end
 
   def destroy
+    #render text: params
     # TODO
+    @product.destroy
+    redirect_to backend_products_path
+
   end
+
+  def find_product
+    @product = Product.find(params[:id]) #抽出商品id
+      
+  end 
 
   def product_params
     params.require(:product).permit(:name, :description, :price, :is_online, :image)
